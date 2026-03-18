323afb20a6ff8f751e2b6519dd60c8b8799207c0
Edexal <220395190+Edexaal@users.noreply.github.com>
2026-03-18 01:01:59 -0500

    Add missing main workflow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from lib.arg import CLIParser, ArgChecker, ArgAssembler
 from lib.file import Writer, FileExecutor, RenpyReader
+from lib.narrator_handler import NarratorHandler
+from lib.validator.speaker import ObjectStrategy
 
 
 def run():
@@ -15,7 +17,10 @@ def run():
     file_executor = FileExecutor()
     reader = RenpyReader()
     file_infos = file_executor.file_lines(reader, arg_namespace.folder_or_file)
-    # TODO: Place narration remover logic here!
+    ObjectStrategy.define_speakers(file_infos)
+    narrator_handler = NarratorHandler()
+    file_infos = narrator_handler.remove(file_infos, arg_namespace)
+    file_executor.write_files(writer, file_infos)
 
 
 if __name__ == "__main__":
